792dbb589810b6ef9335535a404468ff85e83c2d
vijaywargiyariddhima <vijaywargiya.riddhima@gmail.com>
2026-02-05 19:48:58 +0530

    Delete Band Name Generator Project - 1.py

--- Band Name Generator Project - 1.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Generator")
-city = input("What is the name of the city you grew up in?\n")
-pet = input("What is the name of your pet?\n")
-print("Your band name could be: " + city + " " + pet)
